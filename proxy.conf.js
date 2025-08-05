7fddae6a742ad9eefbb281ed495e2e17747cddae
gabriel geraldo <gabrieldomingosgeraldo@gmail.com>
2025-08-05 10:26:28 -0300

    commite antes atualização para ver 19 do angular

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,6 +1,6 @@
 const PROXY_CONFIG = {
   "/api": {
-      "target": "http://192.168.1.35:1234/app-root",
+      "target": "http://localhost:1234/app-root",
       "secure": true,
       "bypass": function (req, res, proxyOptions) {
           req.headers["Authorization"] = "Basic Z2FicmllbGdlcmFsZG86Z2FicmllbA==";
