f2497d0a7f89262a7833a9aeda4248df86524ca1
gabriel geraldo <gabrieldomingosgeraldo@gmail.com>
2025-08-05 08:24:27 -0300

    atualização para angular 15

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,10 +1,9 @@
 const PROXY_CONFIG = {
   "/api": {
-      "target": "http://192.168.1.34:1234/app-root",
+      "target": "http://192.168.1.35:1234/app-root",
       "secure": true,
       "bypass": function (req, res, proxyOptions) {
-          // req.headers["Authorization"] = "Basic Z2FicmllbGdlcmFsZG86R2FicmllbCQ0MDc0";
-          req.headers["Authorization"] = "Basic am9obkBleGFtcGxlLmNvbTphYmMxMjM=";
+          req.headers["Authorization"] = "Basic Z2FicmllbGdlcmFsZG86Z2FicmllbA==";
       }
   }
 }
